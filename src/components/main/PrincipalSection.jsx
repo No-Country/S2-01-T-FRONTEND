55d7d509e492b2cb6da9aca2622fa08deff46a4d
ezSan <ezequiel.sanchez.7391@gmail.com>
2022-06-02 15:58:21 -0300

    Responsive Main

--- src/components/main/PrincipalSection.jsx
+++ src/components/main/PrincipalSection.jsx
@@ -2,27 +2,36 @@ import React from "react";
 import styled from "styled-components";
 import Button from '@mui/material/Button';
 import Logo from "../../assets/img/logo.png";
-import TiendaPagos from "../../assets/img/tienda-pagos.png"
+import TiendaPagos from "../../assets/img/tienda-pagos.png";
+import Grid from '@mui/material/Grid';
 
 import Container from '@mui/material/Container';
 
 function PrincipalSection() {
-    
+
     return (
         <PrincipalSectionStyled>
-            
-            <Container className="leftContainer">
-                <Container className="titleAndParagraph">
-                    <h2>elFiado.com</h2>
-                    <p>Conocé mas acerca del comportamiento crediticio <br></br>de tus clientes y viceversa</p>
-                </Container>
-                <Container className="registerAndHome">
-                    <Button color="warning" variant="contained" className="registerBtn" href="/register">Registrate! </Button>
-                    <img src={Logo} alt="LogoIcon" />
-                </Container>
-            </Container>
 
-            <img className='tiendaPagosImg' src={TiendaPagos} alt='Tienda Pagos Img' />
+            <Grid container>
+
+                <Grid item xs={12} md={6}  xl={6}>
+                    <Container className="titleAndParagraph">
+                        <h2>elFiado.com</h2>
+                        <p>Conocé mas acerca del comportamiento crediticio <br></br>de tus clientes y viceversa</p>
+                    </Container>
+                    <Container className="registerAndHome">
+                        <Button color="warning" variant="contained" className="registerBtn" href="/register">Registrate! </Button>
+                        <img src={Logo} alt="LogoIcon" />
+                    </Container>
+                </Grid>
+
+                <Grid item xs={12} md={6} xl={6} className="imgPrincipal">
+                    <img className='tiendaPagosImg' src={TiendaPagos} alt='Tienda Pagos Img' />    
+                </Grid>
+
+
+            </Grid>
+
 
         </PrincipalSectionStyled>
     )
@@ -32,31 +41,31 @@ function PrincipalSection() {
 const PrincipalSectionStyled = styled.div`
 
     margin:1em;
-    display:flex;
+    
+    justify-content:center;
+    align-items:center;
 
-    .leftContainer{
-        width:50%;
-    } 
-
+    img{
+        width:80%;
+        margin-left:10%;
+        margin-top:1em;
+    }
+ 
     .registerAndHome{
         display:flex;
         flex-direction:column;
         width:50%;
         margin-left:auto;
         margin-right:auto;
-    }  
-
-    .registerBtn{
-        width:70%;
-        margin-left:auto;
-        margin-right:auto;
-    }
-
-    .tiendaPagosImg{
-        width:45%;
     } 
 
+    .registerBtn{
+        font-size:small;
+    }
 
+   
+
+    
 
 `
 
