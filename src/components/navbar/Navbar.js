15eeaef5466fccca0d910b13ffa3b19f383e0ae5
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2022-06-13 20:11:12 -0300

    Reorganizando Rutas

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import logo from "../../assets/img/logo.png";
 import { Barra } from "../navbar/styles/elementsNavbar";
 import { NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Button } from "@mui/material";
 import { List, Drawer, ListItemButton, ListItemText } from "@mui/material";
+import { UserContext } from '../../providers/UserProvider'
 
 const Navbar = () => {
-  const [user, setUser] = useState(false);
+
+  const { isLogged, setIsLogged } = useContext(UserContext)
+
   const [open, setOpen] = useState(false);
   return (
     <>
@@ -17,47 +20,59 @@ const Navbar = () => {
             <img className="icono_img" src={logo} alt="logo" />
           </div>
         </NavLink>
-        <ul className="container_options">
-          <NavLink to="/app/search" style={{ textDecoration: "none" }}>
-            <li className="options">Buscar</li>
-          </NavLink>
-          <NavLink to="/app/ayuda" style={{ textDecoration: "none" }}>
-            <li className="options">Ayuda</li>
-          </NavLink>
-          <NavLink to="/" style={{ textDecoration: "none" }}>
-            <li className="options">Contacto</li>
-          </NavLink>
-          <NavLink to="/about" style={{ textDecoration: "none" }}>
-            <li className="options">Quienes Somos</li>
-          </NavLink>
-        </ul>
-        {user ? (
-          <div className="container_buttons">
-            <Button
-              variant="contained"
-              onClick={() => setUser(!user)}
-              className="buttons"
-            >
-              Logout
-            </Button>
-          </div>
-        ) : (
-          <div className="container_buttons">
-            <NavLink to="/auth/register" style={{ textDecoration: "none" }}>
-              <Button variant="contained" className="buttons">
-                Registrarse
-              </Button>
-            </NavLink>
-            <NavLink to="/auth/login" style={{ textDecoration: "none" }}>
+        {isLogged ? (
+          <>
+            <ul className="container_options">              
+              <NavLink to="/app/search" style={{ textDecoration: "none" }}>
+                <li className="options">Buscar</li>
+              </NavLink>
+              <NavLink to="/" style={{ textDecoration: "none" }}>
+                <li className="options">Contacto</li>
+              </NavLink>
+              <NavLink to="/app/ayuda" style={{ textDecoration: "none" }}>
+                <li className="options">Ayuda</li>
+              </NavLink>
+              <NavLink to="/about" style={{ textDecoration: "none" }}>
+                <li className="options">Quienes Somos</li>
+              </NavLink>
+            </ul>
+            <div className="container_buttons">
               <Button
-                className="buttons"
                 variant="contained"
-                onClick={() => setUser(!user)}
+                onClick={() => setIsLogged(!isLogged)}
+                className="buttons"
               >
-                Acceder
+                Logout
               </Button>
-            </NavLink>
-          </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <ul className="container_options">
+              <NavLink to="/" style={{ textDecoration: "none" }}>
+                <li className="options">Contacto</li>
+              </NavLink>
+              <NavLink to="/about" style={{ textDecoration: "none" }}>
+                <li className="options">Quienes Somos</li>
+              </NavLink>
+            </ul>
+            <div className="container_buttons">
+              <NavLink to="/auth/register" style={{ textDecoration: "none" }}>
+                <Button variant="contained" className="buttons">
+                  Registrarse
+                </Button>
+              </NavLink>
+              <NavLink to="/auth/login" style={{ textDecoration: "none" }}>
+                <Button
+                  className="buttons"
+                  variant="contained"
+                  onClick={() => setIsLogged(!isLogged)}
+                >
+                  Acceder
+                </Button>
+              </NavLink>
+            </div>
+          </>
         )}
         <div className="burger">
           <Button>
@@ -71,42 +86,36 @@ const Navbar = () => {
             anchor="right"
             open={open}
             onClose={() => setOpen(false)}
-            sx={{paperAnchorRight:"red"}}
-            
+            sx={{ paperAnchorRight: "red" }}
           >
-            <List sx={{background:"red"}}className="list">
+            <List sx={{ background: "red" }} className="list">
               {
                 <ListItemButton
                   style={{ display: "flex", flexDirection: "column" }}
                   className="dropdown"
                   onClick={() => setOpen(false)}
-                  
                 >
-                   <NavLink to="/auth/login" style={{ textDecoration: "none",color:"white" }}>
+                  <NavLink to="/auth/login" style={{ textDecoration: "none", color: "white" }}>
                     <ListItemText>Acceder</ListItemText>{" "}
                   </NavLink>
                   <NavLink
                     to="/auth/register"
-                    style={{ textDecoration: "none",color:"white" }}
+                    style={{ textDecoration: "none", color: "white" }}
                   >
                     <ListItemText>Registrarse</ListItemText>{" "}
                   </NavLink>
-                  <NavLink to="/app/search" style={{ textDecoration: "none",color:"white"}}>
+                  <NavLink to="/app/search" style={{ textDecoration: "none", color: "white" }}>
                     <ListItemText >Buscar</ListItemText>{" "}
                   </NavLink>
-                  <NavLink to="/about" style={{ textDecoration: "none",color:"white" }}>
+                  <NavLink to="/about" style={{ textDecoration: "none", color: "white" }}>
                     <ListItemText>Quienes Somos</ListItemText>{" "}
                   </NavLink>
-                  <NavLink to="/" style={{ textDecoration: "none",color:"white" }}>
+                  <NavLink to="/" style={{ textDecoration: "none", color: "white" }}>
                     <ListItemText>Contacto</ListItemText>
                   </NavLink>
-                  <NavLink to="/app/ayuda" style={{ textDecoration: "none",color:"white" }}>
+                  <NavLink to="/app/ayuda" style={{ textDecoration: "none", color: "white" }}>
                     <ListItemText>Ayuda</ListItemText>
                   </NavLink>
-                 
-                 
-                 
-                 
                 </ListItemButton>
               }
             </List>
